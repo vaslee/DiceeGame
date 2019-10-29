7faa69e980509537e1e32b981b1b17dd2138bffe
vaslee <ltx0503@gmail.com>
2019-10-29 11:19:39 -0400

    test

---
+++ DiceeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceeGame
+//
+//  Created by TingxinLi on 10/29/19.
+//  Copyright © 2019 TingxinLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
